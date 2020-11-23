bf680efc92886b426432c18c8c3111e78d371013
alexeyRomanchenko <romanchenko.alek@mail.ru>
2020-11-23 16:55:56 +0300

    add multitasking

---
+++ ElectronicLibrary.UI.Desktop/ViewModels/BusinessViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectronicLibrary.UI.Desktop.ViewModels
+{
+    public class BusinessViewModel
+    {
+        public string Description {get;set;}
+        public string Industry { get; set; }
+        public string Size { get; set; }
+    }
+}
